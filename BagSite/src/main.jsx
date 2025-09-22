9ce99880120cfbdc1f91c5b0e3bae525af773b32
bgt1717 <brycetown10@gmail.com>
2025-09-22 00:33:53 -0500

    Gallery image routes and scalability

--- BagSite/src/main.jsx
+++ BagSite/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
+import ImagePage from "./pages/ImagePage"; // new
 import About from "./pages/About";
 import Events from "./pages/Events";
+import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/image/:id" element={<ImagePage />} /> {/* dynamic route */}
         <Route path="/about" element={<About />} />
         <Route path="/events" element={<Events />} />
       </Routes>
--- BagSite/src/pages/ImagePage.jsx
+++ BagSite/src/pages/ImagePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import img1 from "../Pictures/1.jpg";
@@ -9,22 +9,22 @@ import img4 from "../Pictures/4.jpg";
 import img5 from "../Pictures/5.jpg";
 import img6 from "../Pictures/6.jpg";
 
-export default function Gallery() {
+
+export default function ImagePage() {
+  const { id } = useParams();
   const images = [img1, img2, img3, img4, img5, img6];
+  const imageSrc = images[id - 1]; // id is 1-based
 
   return (
     <div className="container">
       <Navbar />
-      <main className="gallery">
-        {images.map((src, idx) => (
-          <Link key={idx} to={`/image/${idx + 1}`}>
-            <img src={src} alt={`Gallery ${idx + 1}`} />
-          </Link>
-        ))}
+      <main className="single-image-page" style={{ textAlign: "center", padding: "2rem" }}>
+        <Link to="/gallery">← Back to Gallery</Link>
+        <div style={{ marginTop: "1rem" }}>
+          <img src={imageSrc} alt={`Gallery ${id}`} style={{ maxWidth: "90%", borderRadius: "10px" }} />
+        </div>
       </main>
       <Footer />
     </div>
   );
 }
-
-
