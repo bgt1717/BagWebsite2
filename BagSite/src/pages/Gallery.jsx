ccd1abaa79eb7d51e0fdc09cc28223f01fa66916
bgt1717 <brycetown10@gmail.com>
2025-09-22 00:22:21 -0500

    zoom images add

--- BagSite/src/pages/Gallery.jsx
+++ BagSite/src/pages/Gallery.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+// Import images
+import img1 from "../Pictures/1.jpg";
+import img2 from "../Pictures/2.jpg";
+import img3 from "../Pictures/3.jpg";
+import img4 from "../Pictures/4.jpg";
+import img5 from "../Pictures/5.jpg";
+import img6 from "../Pictures/6.jpg";
+
 export default function Gallery() {
-  const images = [
-    "https://source.unsplash.com/random/400x300?nature",
-    "https://source.unsplash.com/random/400x300?city",
-    "https://source.unsplash.com/random/400x300?tech",
-  ];
+  const images = [img1, img2, img3, img4, img5, img6];
 
   return (
     <div className="container">
@@ -21,3 +25,4 @@ export default function Gallery() {
     </div>
   );
 }
+
