2e42a1d80d702fdda65d072399899cc0345b7392
bgt1717 <brycetown10@gmail.com>
2025-09-22 23:23:47 -0500

    icons added

--- BagSite/src/pages/Events.jsx
+++ BagSite/src/pages/Events.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 
 export default function Events() {
   return (
@@ -9,10 +10,23 @@ export default function Events() {
       <main className="events-section">
         <h2>Scheduled Events</h2>
         <ul>
-          <li>Barton Creek Mall Farmer's Market - Sept 25</li>
-          <li>Event 2 - Oct 5</li>
-          <li>Event 3 - Oct 20</li>
+          <li>React Workshop - Sept 25</li>
+          <li>Gallery Launch - Oct 5</li>
+          <li>Community Meetup - Oct 20</li>
         </ul>
+
+        {/* Social icons */}
+        <div className="social-link" style={{ marginTop: "2rem", textAlign: "center", display: "flex", justifyContent: "center", gap: "1.5rem" }}>
+          <a href="https://www.instagram.com/your_instagram_handle" target="_blank" rel="noopener noreferrer" style={{ color: "#E1306C", fontSize: "2rem" }}>
+            <FaInstagram />
+          </a>
+          <a href="https://www.facebook.com/your_facebook_handle" target="_blank" rel="noopener noreferrer" style={{ color: "#4267B2", fontSize: "2rem" }}>
+            <FaFacebook />
+          </a>
+          <a href="https://twitter.com/your_twitter_handle" target="_blank" rel="noopener noreferrer" style={{ color: "#1DA1F2", fontSize: "2rem" }}>
+            <FaTwitter />
+          </a>
+        </div>
       </main>
       <Footer />
     </div>
